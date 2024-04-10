93f2f19bb285e48047dcd3df098425dafa4d7dde
THE0R <rtbujak@gmail.com>
2024-04-10 17:05:22 -0400

    added creative mode tab

--- src/main/java/net/rbujak/tutorialmod/TutorialMod.java
+++ src/main/java/net/rbujak/tutorialmod/TutorialMod.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.rbujak.tutorialmod.item.ModCreativeModeTabs;
 import net.rbujak.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -29,6 +30,8 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
 
---
+++ src/main/java/net/rbujak/tutorialmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,30 @@
+package net.rbujak.tutorialmod.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.rbujak.tutorialmod.TutorialMod;
+
+import java.awt.*;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.EYE_OF_HELENA.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.EYE_OF_HELENA.get());
+                        pOutput.accept(ModItems.HELMET_OF_HELENA.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
